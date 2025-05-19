05e2981810f2a2a2e3e22536d65c74dc77891437
Salvatore <161577708+Salvatorevil03@users.noreply.github.com>
2025-05-19 10:27:19 +0200

    Add files via upload

---
+++ DB/SQLTask.sql
@@ -0,0 +1,113 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema taskdidattici
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema taskdidattici
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `taskdidattici` DEFAULT CHARACTER SET utf8mb3 ;
+USE `taskdidattici` ;
+
+-- -----------------------------------------------------
+-- Table `taskdidattici`.`docente`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `taskdidattici`.`docente` (
+  `id` INT NOT NULL,
+  `nome` VARCHAR(45) NULL DEFAULT NULL,
+  `cognome` VARCHAR(45) NULL DEFAULT NULL,
+  `mail` VARCHAR(45) NULL DEFAULT NULL,
+  `password` VARCHAR(45) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `taskdidattici`.`classe`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `taskdidattici`.`classe` (
+  `codice` INT NOT NULL,
+  `nome` VARCHAR(45) NULL DEFAULT NULL,
+  `numeroTask` INT NULL DEFAULT NULL,
+  `docente_id` INT NOT NULL,
+  PRIMARY KEY (`codice`, `docente_id`),
+  INDEX `fk_classe_docente1_idx` (`docente_id` ASC) VISIBLE,
+  CONSTRAINT `fk_classe_docente1`
+    FOREIGN KEY (`docente_id`)
+    REFERENCES `taskdidattici`.`docente` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `taskdidattici`.`studente`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `taskdidattici`.`studente` (
+  `id` INT NOT NULL,
+  `nome` VARCHAR(45) NULL DEFAULT NULL,
+  `cognome` VARCHAR(45) NULL DEFAULT NULL,
+  `mail` VARCHAR(45) NULL DEFAULT NULL,
+  `password` VARCHAR(45) NULL DEFAULT NULL,
+  `numTaskCompletati` INT NULL DEFAULT NULL,
+  `numTaskValutati` INT NULL DEFAULT NULL,
+  `punteggioTotaleOttenuto` INT NULL DEFAULT NULL,
+  `classe_codice` INT NOT NULL,
+  PRIMARY KEY (`id`, `classe_codice`),
+  INDEX `fk_studente_classe1_idx` (`classe_codice` ASC) VISIBLE,
+  CONSTRAINT `fk_studente_classe1`
+    FOREIGN KEY (`classe_codice`)
+    REFERENCES `taskdidattici`.`classe` (`codice`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `taskdidattici`.`task`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `taskdidattici`.`task` (
+  `id` INT NOT NULL,
+  `titolo` VARCHAR(45) NULL DEFAULT NULL,
+  `descrizione` VARCHAR(150) NULL DEFAULT NULL,
+  `dataScadenza` VARCHAR(8) NULL DEFAULT NULL,
+  `maxPuntiAssegnabili` INT NULL DEFAULT NULL,
+  `classe_codice` INT NOT NULL,
+  PRIMARY KEY (`id`, `classe_codice`),
+  INDEX `fk_task_classe1_idx` (`classe_codice` ASC) VISIBLE,
+  CONSTRAINT `fk_task_classe1`
+    FOREIGN KEY (`classe_codice`)
+    REFERENCES `taskdidattici`.`classe` (`codice`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `taskdidattici`.`consegna`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `taskdidattici`.`consegna` (
+  `id` INT NOT NULL,
+  `punteggio` INT NULL DEFAULT NULL,
+  `soluzione` BLOB NULL DEFAULT NULL,
+  `task_id` INT NOT NULL,
+  `studente_id` INT NOT NULL,
+  PRIMARY KEY (`id`, `task_id`, `studente_id`),
+  INDEX `fk_consegna_task1_idx` (`task_id` ASC) VISIBLE,
+  INDEX `fk_consegna_studente1_idx` (`studente_id` ASC) VISIBLE,
+  CONSTRAINT `fk_consegna_studente1`
+    FOREIGN KEY (`studente_id`)
+    REFERENCES `taskdidattici`.`studente` (`id`),
+  CONSTRAINT `fk_consegna_task1`
+    FOREIGN KEY (`task_id`)
+    REFERENCES `taskdidattici`.`task` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
