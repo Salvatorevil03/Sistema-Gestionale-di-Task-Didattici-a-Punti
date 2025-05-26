c5973a4606fa87c5bf6b07c069d5a608a8a5b0cd
Apaticz <ernestocifuni29@gmail.com>
2025-05-26 10:06:53 +0200

    Add files via upload

--- src/entity/EntityClasse.java
+++ src/entity/EntityClasse.java
@@ -2,18 +2,22 @@ package entity;
 
 import database.DBClasse;
 
+import java.util.ArrayList;
+
 public class EntityClasse {
     private int codice;
     private String nome;
     private int numeroTask;
     private EntityDocente docente;
-    //private ArrayList<EntityStudente> studenti;
-    //private ArrayList<EntityTask> task;
+    private ArrayList<EntityStudente> studenti;
+    private ArrayList<EntityTask> task;
     //RICORDA LA CREAZIONE DI QUESTI ATTRIBUTI -> MODIFICA DEL TO STRING
 
 
     public EntityClasse() {
         super();
+        this.studenti = new ArrayList<EntityStudente>();
+        this.task = new ArrayList<EntityTask>();
     }
 
     public EntityClasse(int codice) {
@@ -21,20 +25,46 @@ public class EntityClasse {
         this.codice = codice;
         this.nome = classe.getNome();
         this.numeroTask = classe.getNumeroTask();
+        this.studenti= new ArrayList<EntityStudente>();
+        this.task=new ArrayList<EntityTask>();
 
         classe.caricaDocenteDaDB();
         caricaDocente(classe);
+        classe.caricaStudentiDaDB();
+        caricaStudenti(classe);
+        classe.caricaTaskDaDB();
+        caricaTask(classe);
     }
 
     public EntityClasse(DBClasse classe) {
         this.codice = classe.getCodice();
         this.nome = classe.getNome();
         this.numeroTask = classe.getNumeroTask();
+
+        classe.caricaDocenteDaDB();
+        caricaDocente(classe);
+        classe.caricaStudentiDaDB();
+        caricaStudenti(classe);
+        classe.caricaTaskDaDB();
+        caricaTask(classe);
     }
 
     public void caricaDocente(DBClasse classe) {
         EntityDocente docente = new EntityDocente(classe.getDocente());
         this.docente = docente;
+    }
+
+    public void caricaStudenti(DBClasse classe) {
+        for(int i=0; i<classe.getStudenti().size(); i++) {
+            EntityStudente studente = new EntityStudente(classe.getStudenti().get(i));
+            this.studenti.add(studente);
+        }
+    }
+    public void caricaTask(DBClasse classe) {
+        for(int i=0; i<classe.getTask().size(); i++) {
+            EntityTask task = new EntityTask(classe.getTask().get(i));
+            this.task.add(task);
+        }
     }
 
     //GETTER AND SETTER-----------------
@@ -56,6 +86,24 @@ public class EntityClasse {
     public void setNumeroTask(int numeroTask) {
         this.numeroTask = numeroTask;
     }
+    public EntityDocente getDocente() {
+        return docente;
+    }
+    public void setDocente(EntityDocente docente) {
+        this.docente = docente;
+    }
+    public ArrayList<EntityStudente> getStudenti() {
+        return studenti;
+    }
+    public void setStudenti(ArrayList<EntityStudente> studenti) {
+        this.studenti = studenti;
+    }
+    public ArrayList<EntityTask> getTask() {
+        return task;
+    }
+    public void setTask(ArrayList<EntityTask> task) {
+        this.task = task;
+    }
 
     @Override
     public String toString() {
@@ -64,6 +112,11 @@ public class EntityClasse {
                 ", nome='" + nome + '\'' +
                 ", numeroTask=" + numeroTask +
                 ", docente=" + docente +
+                ", studenti=" + studenti +
+                ", task=" + task +
                 '}';
     }
+
+
+
 }
