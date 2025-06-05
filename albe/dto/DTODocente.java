549408c6fe442160e30afdd482f8f016088c06db
ElPaposky <albertobonventre00@gmail.com>
2025-06-05 08:39:35 +0200

    Add files via upload

---
+++ albe/dto/DTODocente.java
@@ -0,0 +1,9 @@
+package dto;
+
+public class DTODocente extends DTOUtente {
+
+    public DTODocente(int id, String nome, String cognome, String mail, String password) {
+        super(id, nome, cognome, mail, password);
+    }
+
+}
