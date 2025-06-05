dc3744a2af44ffcc4f77bdddcd706e78ceb9fbde
ElPaposky <albertobonventre00@gmail.com>
2025-06-05 22:44:45 +0200

    Delete albe directory

--- albe/dto/DTODocente.java
+++
@@ -1,9 +0,0 @@
-package dto;
-
-public class DTODocente extends DTOUtente {
-
-    public DTODocente(int id, String nome, String cognome, String mail, String password) {
-        super(id, nome, cognome, mail, password);
-    }
-
-}
