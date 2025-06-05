e733dcd9837de70c39b02593fdd74c7d49cc5ea9
Salvatore <161577708+Salvatorevil03@users.noreply.github.com>
2025-06-05 10:15:14 +0200

    Add files via upload

---
+++ src/boundary/GUIListaStudentiClasse.java
@@ -0,0 +1,140 @@
+package taskdidatticiNEW;
+
+import controller.Controller;
+import dto.DTOStudente;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
+import javax.swing.border.LineBorder;
+
+
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ * Classe che rappresenta la finestra GUI per la lista dei studenti della classe.
+ * <br>Il docente che ha creato la classe potrà accedere a questa GUI a partire
+ * dalla GUI {@link GUIClasseDocente}.
+ * <br>In questa finestra il docente potrà visualizzare la lista dei studenti che
+ * si sono iscritti alla classe selezionata o in maniera autonoma o mediante la
+ * funzione di iscrizione a una classe che si trova nella GUI {@link GUIDocente}.
+ */
+public class GUIListaStudentiClasse extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTable table;
+	private DefaultTableModel model; /////
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUIListaStudentiClasse frame = new GUIListaStudentiClasse();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public GUIListaStudentiClasse() {
+		SessioneDocente docente = SessioneDocente.getInstance();
+		String pkClasseSelezionata = docente.getPkClasseSelezionata();
+		ArrayList<DTOStudente> studenti = Controller.getStudenti(pkClasseSelezionata);
+
+		//
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 900, 650);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(213, 241, 247));
+		contentPane.setBorder(new LineBorder(new Color(0, 0, 0)));
+		//
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		
+		JButton aggiornaButton = new JButton("aggiorna");
+		aggiornaButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		aggiornaButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				GUIListaStudentiClasse seconda = new GUIListaStudentiClasse(); /// Crea nuova finestra
+				seconda.setVisible(true); /// Mostra nuova finestra
+				dispose(); /// Chiude questa finestra
+			}
+		});
+		aggiornaButton.setBounds(785, 11, 89, 23);
+		contentPane.add(aggiornaButton);
+		
+		JButton indietroBtn = new JButton("indietro");
+		indietroBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		indietroBtn.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				GUIClasseDocente seconda = new GUIClasseDocente(); /// Crea nuova finestra
+				seconda.setVisible(true); /// Mostra nuova finestra
+				dispose(); /// Chiude questa finestra
+			}
+		});
+		indietroBtn.setBounds(10, 11, 89, 23);
+		contentPane.add(indietroBtn);
+		
+		JLabel lblBenvenuto = new JLabel("Lista Studenti della classe");
+		lblBenvenuto.setHorizontalAlignment(SwingConstants.CENTER);
+		lblBenvenuto.setFont(new Font("Tahoma", Font.BOLD, 30));
+		lblBenvenuto.setBounds(66, 69, 708, 56);
+		contentPane.add(lblBenvenuto);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(83, 151, 713, 377);
+		contentPane.add(scrollPane);
+		
+		table = new JTable();
+		model = new DefaultTableModel(
+				new Object[][] {}, /// Inizia vuoto
+				new String[] { "ID","Nome", "Cognome" } /// Nome colonna
+			){
+			@Override
+			public boolean isCellEditable(int row, int column) {
+				return false;
+			}
+		};
+		table.setModel(model);
+		if (!studenti.isEmpty()){
+			for (DTOStudente s : studenti) {
+				model.addRow(new Object[] { s.getId(),s.getNome(),s.getCognome() });
+			}
+		}
+		scrollPane.setViewportView(table);
+	}
+}
